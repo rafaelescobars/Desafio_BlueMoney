b9c084d13888c1816be6b789da6f3eebb950664d
Rafael Escobar Salas <rafael.escobar.s@gmail.com>
2021-11-29 22:37:18 -0300

    preliminar delivery

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ for (let value in parameters) {
   i++
 }
 
+let newFileName = parameters.newFileName
+let newFileExtension = parameters.newFileExtension
+let pesosQuantity = parameters.pesosQuantity
+let economicIndex = parameters.economicIndex
 
+//definir función que retorne template
+const templateReturn = (pesosQuantity, economicIndex, conversion) => {
+  return `A la fecha: ${new Date}\nFue realizada la cotización con los siguientes datos:\nCantidad de pesos a convertir: ${pesosQuantity} pesos\nConvertido a "${economicIndex}" da un total de: ${conversion}`
+}
 
 // definir https
 const https = require('https');
@@ -28,10 +36,10 @@ https.get(`https://mindicador.cl/api`, resp => {
     resp.on('data', data => {
       let myData = JSON.parse(data);
       let exchangeRate = myData[`${parameters.economicIndex}`].valor
-      let conversion = (parameters.pesosQuantity / exchangeRate).toFixed(2);
+      let conversion = (pesosQuantity / exchangeRate).toFixed(2);
 
-      fs.writeFile(`${parameters.newFileName}.${parameters.newFileExtension}`, `A la fecha: ${new Date}\nFue realizada la cotización con los siguientes datos:\nCantidad de pesos a convertir: ${parameters.pesosQuantity} pesos\nConvertido a "${parameters.economicIndex}" da un total de: ${conversion}`, `utf8`, () => {
-        fs.readFile(`${parameters.newFileName}.${parameters.newFileExtension}`, `utf8`, (err, readFileData) => {
+      fs.writeFile(`${newFileName}.${newFileExtension}`, templateReturn(pesosQuantity, economicIndex, conversion), `utf8`, () => {
+        fs.readFile(`${newFileName}.${newFileExtension}`, `utf8`, (err, readFileData) => {
           console.log(readFileData);
         })
       })
